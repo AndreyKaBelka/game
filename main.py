74170fe9f77278069fd37eaa044a8bf582a32394
Andrey Ovchinnikov <dream.of.yaz@gmail.com>
2019-06-03 00:37:58 +0600

    init window

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import pygame
 
 
 class Game:
-    def __init__(self):
-        self.width = 1000
-        self.height = 700
-        self.win = pygame.display.set_mode((self.width, self.height))
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.win = pygame.display.set_mode([self.width, self.height], flags=pygame.RESIZABLE)
 
     def run(self):
         run = True
-
         while run:
-            for event in pygame.event():
+            self.win.fill([255, 255, 255])
+            pygame.display.update()
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    run = False
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
 
+                if event.type == pygame.VIDEORESIZE:
+                    self.win = pygame.display.set_mode((event.w, event.h),
+                                                       pygame.RESIZABLE)
         pygame.quit()
 
 
-g = Game()
+g = Game(1000, 700)
 g.run()
