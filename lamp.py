ee26b76e2e2116365f4a919b0887cf1582bc03a3
Andrey Ovchinnikov <dream.of.yaz@gmail.com>
2019-06-07 00:50:13 +0600

    Added some pictures
    Signed-off-by: Andrey Ovchinnikov <dream.of.yaz@gmail.com>

--- lamp.py
+++ lamp.py
@@ -1,2 +1,45 @@
+import pygame
+import os
 
-class Lamp(pygame.sprite.Sprite):
+
+class Lamp:
+    def __init__(self, x, y, charge):
+        self.lamp = pygame.image.load(os.path.join("game_assets", "lightbulb_on.png"))
+        self.lamp = pygame.transform.scale(self.lamp, [50, 50])
+        self.pos = x, y
+        self.charge = charge
+        self.connected = []
+        '''
+        for connect in connected:
+            if not (connect == self.pos):
+                self.connected.append(connect)
+        '''
+        self.lamp_rect = self.lamp.get_rect(center=(self.pos[0], self.pos[1]))
+
+    def draw(self, win):
+        """
+        Draw a lamp
+        :param win: Surface
+        :return:
+        """
+        for line in self.connected:
+            if line.pos != self.pos:
+                pygame.draw.line(win, (0, 0, 0), (self.pos[0], self.pos[1]), (line.pos[0], line.pos[1]))
+
+    def give(self):
+        """
+
+        :return: None
+        """
+        self.charge -= len(self.connected)
+        for lamp in self.connected:
+            lamp.charge += 1
+
+    def take(self):
+        """
+
+        :return: None
+        """
+        self.charge += len(self.connected)
+        for lamp in self.connected:
+            lamp.charge -= 1
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
-import Lamp
+from lamp import Lamp
+from random import randint
 
 
 class Game:
@@ -9,12 +10,43 @@ class Game:
         self.height = height
         self.win = pygame.display.set_mode([self.width, self.height], flags=pygame.RESIZABLE)
         self.bg = pygame.image.load(os.path.join("game_assets", "bg.png"))
+        self.pos = [(361, 524), (875, 520), (639, 189), (500, 189)]
+        self.lamps = []
+        self.connected = [[1, 2, 3], [0, 2], [0, 1], [0]]
+
+        for dot in self.pos:
+            self.lamps.append(Lamp(dot[0], dot[1], randint(1, 3)))
+        count = 0
+        for lampa in self.lamps:
+            for i in range(len(self.connected[count])):
+                lampa.connected.append(self.lamps[self.connected[count][i]])
+            count += 1
+
+        pygame.font.init()
+        self.myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
+    def draw(self):
+        self.win.blit(self.bg, [0, 0])
+        for la in self.lamps:
+            la.draw(self.win)
+        for la in self.lamps:
+            self.win.blit(la.lamp, la.lamp_rect)
+            text = self.myfont.render(str(la.charge), False, (0, 0, 0))
+            text_rect = text.get_rect(center=la.pos)
+            self.win.blit(text, text_rect)
 
     def run(self):
         clock = pygame.time.Clock()
+        dbclock = pygame.time.Clock()
+
+        double_click_event = pygame.USEREVENT + 1
+        timer = 0
+        global timerset
+
         while 1:
             clock.tick(60)
             pygame.display.update()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -28,11 +60,41 @@ class Game:
                     self.height = event.h
                     self.win = pygame.display.set_mode([self.width, self.height], flags=pygame.RESIZABLE)
                     self.bg = pygame.transform.scale(self.bg, [event.w, event.h])
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos_mouse = (event.pos[0], event.pos[1])
+                    global press
+                    press = False
+                    for la in self.lamps:
+                        global lamp
+                        if la.lamp_rect.collidepoint(pos_mouse):
+                            lamp = la
+                            press = True
+                            if timer == 0:
+                                pygame.time.set_timer(double_click_event, 250)
+                                timerset = True
+                            elif timer == 1:
+                                pygame.time.set_timer(double_click_event, 0)
+                                # double click function
+                                print("double")
+                                la.give()
+                                timerset = False
+
+                            if timerset:
+                                timer = 1
+                            else:
+                                timer = 0
+
+                elif event.type == double_click_event:
+                    if press:
+                        # single click
+                        pygame.time.set_timer(double_click_event, 0)
+                        timer = 0
+                        print("single click")
+                        lamp.take()
 
                 self.draw()
 
-    def draw(self):
-        self.win.blit(self.bg, [0, 0])
 
-g = Game(1000, 700)
-g.run()
+if __name__ == "__main__":
+    g = Game(1280, 720)
+    g.run()
